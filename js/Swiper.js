514ad048f01f1f1493e7669e5f865307e053e577
Said Emmanuel Castillo Marin <said557@outlook.es>
2023-04-18 20:09:00 -0600

    Bootstrap y mejora de interfaz

---
+++ js/Swiper.js
@@ -0,0 +1,43 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper(".swiper-hero", {
+    // Optional parameters
+    // slidesPerView: "auto",
+    // spaceBetween: 15,
+    // slidesPerGroupAuto: true,
+
+    direction: "horizontal",
+    loop: true,
+    // allowTouchMove: true,
+    // effect: "cube",
+    autoplay: {
+      delay: 10000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      // type: "progressbar"
+      clickable: true,
+      // dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    //   draggable: true,
+    // },
+  });
